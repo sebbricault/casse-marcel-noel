3138480a3151ce92896a11ebe0ac04ef78d62976
sebbricault <seb@MacBook-Air-de-seb.local>
2023-08-01 09:03:55 +0200

    modification PasswordToggle

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./js/App";
 import Password from "./js/PasswordToggle";
 
 const root = document.getElementById("root");
 if (root) {
-  const reactRoot = ReactDOM.createRoot(root);
+  const reactRoot = createRoot(root);
   reactRoot.render(<App />);
 }
 const password = document.getElementById("password");
 if (password) {
-  const reactRoot = ReactDOM.createRoot(password);
+  const reactRoot = createRoot(password);
   reactRoot.render(<Password />);
 }
--- assets/js/PasswordToggle.tsx
+++ assets/js/PasswordToggle.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
-const { useState } = React;
+import "font-awesome/css/font-awesome.min.css"; // Importez le CSS de font-awesome
 
 const PasswordToggle: React.FC = () => {
-  const [isHidden, setIsHidden] = useState(true);
+  const [isHidden, setIsHidden] = React.useState(true);
 
   const togglePasswordVisibility = () => {
-    const passwordInput = document.querySelector("input[type=password]");
+    const passwordInput = document.querySelector(
+      "#registration_form_plainPassword"
+    );
+
     if (passwordInput) {
       if (isHidden) {
         passwordInput.setAttribute("type", "text");
       } else {
         passwordInput.setAttribute("type", "password");
-        console.log("oui");
       }
       setIsHidden(!isHidden);
     }
@@ -22,7 +24,15 @@ const PasswordToggle: React.FC = () => {
       onClick={togglePasswordVisibility}
       className="btn btn-outline-secondary"
     >
-      {isHidden ? "Show" : "Hide"}
+      {isHidden ? (
+        <>
+          <i className="fa fa-eye-slash"></i> Hide
+        </>
+      ) : (
+        <>
+          <i className="fa fa-eye"></i> Show
+        </>
+      )}
     </button>
   );
 };
