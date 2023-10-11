3fc27bfd27eef17963a9037d3ea8f012ab2cff5d
seb <seb.bricault@gmail.com>
2023-10-11 09:03:19 +0200

    mise en place connexion

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./js/App";
 import Password from "./js/PasswordToggle";
-
+import HomePage from "./js/HomePage";
 const root = document.getElementById("root");
 if (root) {
   const reactRoot = createRoot(root);
@@ -12,4 +12,9 @@ const password = document.getElementById("password");
 if (password) {
   const reactRoot = createRoot(password);
   reactRoot.render(<Password />);
+}
+const home = document.getElementById("react-root");
+if (home) {
+  const reactRoot = createRoot(home);
+  reactRoot.render(<HomePage />);
 }
---
+++ assets/js/HomePage.tsx
@@ -0,0 +1,102 @@
+// assets/components/HomePage.tsx
+
+import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="container-fluid bg-light py-5">
+      <header className="d-flex justify-content-between align-items-center py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <i className="fas fa-car-side fa-2x mr-2"></i>
+          <span className="fs-4">Casse Automobile</span>
+        </a>
+      </header>
+      <main>
+        <div className="row">
+          <div className="col-md-6 mb-4">
+            <div className="card">
+              <img src="" alt="Véhicules" className="card-img-top" />
+              <div className="card-body">
+                <h2 className="card-title">
+                  <i className="fas fa-car mr-2"></i>Gestion des véhicules
+                </h2>
+                <p className="card-text">
+                  Accédez à la liste complète des véhicules, ajoutez de nouveaux
+                  véhicules, modifiez ou supprimez des entrées existantes.
+                </p>
+                <a href="/gestion-vehicules" className="btn btn-primary">
+                  Voir plus
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 mb-4">
+            <div className="card">
+              <img src="" alt="Pièces" className="card-img-top" />
+              <div className="card-body">
+                <h2 className="card-title">
+                  <i className="fas fa-cogs mr-2"></i>Gestion des pièces
+                </h2>
+                <p className="card-text">
+                  Accédez à la liste complète des pièces, ajoutez de nouvelles
+                  pièces, modifiez ou supprimez des entrées existantes.
+                </p>
+                <a href="/gestion-pieces" className="btn btn-primary">
+                  Voir plus
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-6 mb-4">
+            <div className="card">
+              <img src="" alt="Transactions" className="card-img-top" />
+              <div className="card-body">
+                <h2 className="card-title">
+                  <i className="fas fa-history mr-2"></i>Historique des
+                  transactions
+                </h2>
+                <p className="card-text">
+                  Consultez l'historique complet des transactions, y compris les
+                  ventes, les achats et les retours.
+                </p>
+                <a href="/historique-transactions" className="btn btn-primary">
+                  Voir plus
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 mb-4">
+            <div className="card">
+              <img
+                src="/path/to/documents-image.jpg"
+                alt="Documents"
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <h2 className="card-title">
+                  <i className="fas fa-file-alt mr-2"></i>Génération de
+                  documents
+                </h2>
+                <p className="card-text">
+                  Générez des factures, des reçus et d'autres documents
+                  importants en fonction des transactions effectuées.
+                </p>
+                <a href="/generation-documents" className="btn btn-primary">
+                  Voir plus
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
